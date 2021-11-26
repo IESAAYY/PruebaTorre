ee3c29d6adab06d67982fa2b306a70751f2dc392
IESAAYY <carlosalmeria@protonmail.com>
2021-11-26 15:21:59 +0100

    9.-

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.torreajedrez;
 
+import java.util.Objects;
+
 public class Posicion {
 
 	// Declaración de variable privates
@@ -45,5 +47,24 @@ public class Posicion {
 			this.columna = columna;
 		}
 	}
+
+	//Métodos hashCode y equals
+	public int hashCode() {
+		return Objects.hash(columna, fila);
+	}
+
+	
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Posicion other = (Posicion) obj;
+		return columna == other.columna && fila == other.fila;
+	}
+	
+	
 
 }
