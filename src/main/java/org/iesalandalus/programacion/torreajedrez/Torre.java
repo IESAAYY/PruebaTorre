6af6340c9d7972836931eb280d170b403f3e16a9
IESAAYY <carlosalmeria@protonmail.com>
2021-11-26 16:25:01 +0100

    12.-

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
@@ -3,4 +3,35 @@ package org.iesalandalus.programacion.torreajedrez;
 public class Torre {
 	private Color Color;
 	private Posicion Posicion;
+	
+	public Color getColor() {
+		return Color;
+	}
+	private void setColor(Color color) {
+		if (color!=null) {
+			Color = color;
+		} else {
+			throw new NullPointerException("Mensaje error: Deberías de haber introducido un color.");
+		}
+		
+		
+		
+	}
+	public Posicion getPosicion() {
+		return Posicion;
+	}
+	private void setPosicion(Posicion posicion) {
+		if (posicion!=null) {
+		Posicion = posicion;
+		} else {
+			throw new NullPointerException("Mensaje error: Deberías de haber introducido una posición.");
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
 }
