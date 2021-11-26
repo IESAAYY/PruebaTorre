4938688794497f443dd23c061197960135544841
IESAAYY <carlosalmeria@protonmail.com>
2021-11-26 20:48:13 +0100

    12.- Corrección de errores.

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
@@ -1,37 +1,34 @@
 package org.iesalandalus.programacion.torreajedrez;
 
 public class Torre {
-	private Color Color;
-	private Posicion Posicion;
 	
+	//Declaración de atributos de clase como privados
+	private Color atributoColor;
+	private Posicion Posicion;
+
 	public Color getColor() {
-		return Color;
+		return atributoColor;
 	}
-	private void setColor(Color color) {
-		if (color!=null) {
-			Color = color;
+	
+	private void setColor(Color sColor) {
+		if (sColor != null) {
+			atributoColor = sColor;
 		} else {
 			throw new NullPointerException("Mensaje error: Deberías de haber introducido un color.");
 		}
-		
-		
-		
+
 	}
+
 	public Posicion getPosicion() {
 		return Posicion;
 	}
+
 	private void setPosicion(Posicion posicion) {
-		if (posicion!=null) {
-		Posicion = posicion;
+		if (posicion != null) {
+			Posicion = posicion;
 		} else {
 			throw new NullPointerException("Mensaje error: Deberías de haber introducido una posición.");
 		}
 	}
-	
-	
-	
-	
-	
-	
-	
+
 }
